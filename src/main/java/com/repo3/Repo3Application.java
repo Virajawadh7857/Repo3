20ed5748eff810f296b2effe6ef0f87f9b13aea3
VIKASH RAJ <virajawadh01@gmail.com>
2025-03-16 22:19:42 +0530

    First commit

---
+++ src/main/java/com/repo3/Repo3Application.java
@@ -0,0 +1,13 @@
+package com.repo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/repo3/Repo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
